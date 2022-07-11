8df511cde21d9afa93d6578354c48db4dd5f49cb
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-11 16:03:13 +0300

    GraduallyDecreasingCarousel

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,43 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    private final DecrementingCarousel carousel;
+    private int currentPosition=0, increasingCoefficient=1;
+
+    public CarouselRun(DecrementingCarousel carousel) {
+        this.carousel=carousel;
+    }
 
     public int next() {
-       throw new UnsupportedOperationException();
+        if (isFinished())
+            return -1;
+
+        if (currentPosition >= carousel.capacity) {
+            currentPosition = 0;
+            increasingCoefficient++;
+        }
+
+        while (carousel.values[currentPosition] < 1){
+            currentPosition++;
+            if (currentPosition >= carousel.capacity) {
+                currentPosition = 0;
+                increasingCoefficient++;
+            }
+        }
+        if (carousel instanceof GraduallyDecreasingCarousel) {
+            int temporary = carousel.values[currentPosition];
+            carousel.values[currentPosition++] -= increasingCoefficient;
+            return temporary;
+        }
+        return carousel.values[currentPosition++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        for (int i:carousel.values) {
+            if (i>0)
+                return false;
+        }
+        return true;
     }
-
 }
+
