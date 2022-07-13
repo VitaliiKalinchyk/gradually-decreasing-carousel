0f1db9f3a510ee5f078c7df1b3c7bae5af6721ac
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-13 13:41:47 +0300

    GraduallyDecreasingCarousel, better solution

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -2,7 +2,6 @@ package com.epam.rd.autotasks;
 
 public class CarouselRun {
     private final DecrementingCarousel carousel;
-    private int currentPosition=0, increasingCoefficient=1;
 
     public CarouselRun(DecrementingCarousel carousel) {
         this.carousel=carousel;
@@ -11,25 +10,7 @@ public class CarouselRun {
     public int next() {
         if (isFinished())
             return -1;
-
-        if (currentPosition >= carousel.capacity) {
-            currentPosition = 0;
-            increasingCoefficient++;
-        }
-
-        while (carousel.values[currentPosition] < 1){
-            currentPosition++;
-            if (currentPosition >= carousel.capacity) {
-                currentPosition = 0;
-                increasingCoefficient++;
-            }
-        }
-        if (carousel instanceof GraduallyDecreasingCarousel) {
-            int temporary = carousel.values[currentPosition];
-            carousel.values[currentPosition++] -= increasingCoefficient;
-            return temporary;
-        }
-        return carousel.values[currentPosition++]--;
+        return carousel.nextResult();
     }
 
     public boolean isFinished() {
@@ -40,4 +21,3 @@ public class CarouselRun {
         return true;
     }
 }
-
--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -1,7 +1,26 @@
 package com.epam.rd.autotasks;
 
 public class GraduallyDecreasingCarousel extends DecrementingCarousel{
+    private int increasingCoefficient = 1;
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
+    }
+    @Override
+    public int nextResult() {
+        if (currentPosition >= capacity) {
+            currentPosition = 0;
+            increasingCoefficient++;
+        }
+
+        while (values[currentPosition] < 1) {
+            currentPosition++;
+            if (currentPosition >= capacity) {
+                currentPosition = 0;
+                increasingCoefficient++;
+            }
+        }
+            int temporary = values[currentPosition];
+            values[currentPosition++] -= increasingCoefficient;
+            return temporary;
     }
 }
